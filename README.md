d4adc1d838718f64e1836ea5f3c25f4225d52cd6
Deren Vural <derenv@live.co.uk>
2021-09-15 02:08:48 +0100

    added ability to create new keys in the various forms specified in Sakaki's guide
    Signed-off-by: Deren Vural <derenv@live.co.uk>

--- README.md
+++ README.md
@@ -6,18 +6,24 @@ Kernel signing utility for easy overall kernel management on Ubuntu systems.
 
 ## Uses
 
-* Signing installed mainline linux kernels using available user EFI keys (allowing them to run with secure boot)
-* Deleting old/invalid signed keys
+* Creating a modified signature database using GPG
+* Signing installed mainline linux kernels (allowing them to run with secure boot)
+* Deleting old/invalid signed kernels
 * Option to reboot when finished
 * Also links to [grub-customizer](https://launchpad.net/~danielrichter2007/+archive/ubuntu/grub-customizer) for easy modification of boot config and [mainline](https://github.com/bkw777/mainline) kernel installer for installing new Ubuntu mainline PPA kernels
 
+## Updating EFI keys & Signature Database
+
+You need to have a directory containing a signature database file on your system - you'll need to create this, i created a "/etc/efikeys" directory as suggested by Sakaki ('chmod -v 700 dirname' to make sure only superuser can access!).
+
+In order to create keys for signing kernels, first generate new keys using the utility and then follow the relevent sections of [Sakaki's EFI Install Guide](https://wiki.gentoo.org/wiki/User:Sakaki/Sakaki's_EFI_Install_Guide/Configuring_Secure_Boot) for appending the keys to your existing keys (I won't go into detail here as the method varies across motherboards - it may be using the efi-updatevar command, it may be by accessing the keystore directly from the BIOS GUI and appending keys from a USB stick, etc). Once exported using efi-readvar, this should create a valid set of key files/certificates.
+
 ## Running
 
-You need to have a directory of EFI keys on your system for this to run!
-
-Set the directories for EFI keys and boot images in the config file and run as sudo.
+Set the directories for your signature database and kernels (eg the folder containing "vmlinuz-5.14.2-051402-generic" or whichever kernel you want to sign, "/boot" on my system) in the config file and run as sudo.
 
 ## Dependencies
 
+* openssl
 * [grub-customizer](https://launchpad.net/~danielrichter2007/+archive/ubuntu/grub-customizer)
 * [mainline](https://github.com/bkw777/mainline)
--- bash-kernel-signer.sh
+++ bash-kernel-signer.sh
@@ -43,8 +43,7 @@ function sign_kernel()
     echo "$prev_out"
     echo "=============================================="
     echo "0 - Exit"
-    echo "which kernel would you like to sign?:"
-    read -r user_input
+    read -r user_input -p "Which kernel would you like to sign?:"
 
     if [[ "$user_input" == "0" ]]; then
       ERROR_MSG="cancelled.."
@@ -90,8 +89,7 @@ function purge_kernel()
     echo "$prev_out"
     echo "=============================================="
     echo "0 - Exit"
-    echo "which signed kernel would you like to purge?:"
-    read -r user_input
+    read -r user_input -p "Which signed kernel would you like to purge?:"
 
     if [[ "$user_input" == "0" ]]; then
       ERROR_MSG="cancelled.."
@@ -108,35 +106,107 @@ function purge_kernel()
 
   return 0
 }
+function create_keys()
+{
+  # Get user input
+  tput reset
+  read -r user_input -p "Please specify (existing) directory for new keys & certificates:"
+
+  # Validate folder exists
+  if [[ "$user_input" == "0" ]]; then
+    ERROR_MSG="cancelled.."
+    return 1
+  elif [[ $(stat -c "%a" "$user_input") == "700" && -w "$user_input" && -r "$user_input" ]]; then
+    # Read old keys
+    echo "reading old keys..."
+    efi-readvar -v PK -o "${user_input}/old_PK.esl"
+    efi-readvar -v KEK -o "${user_input}/old_KEK.esl"
+    efi-readvar -v db -o "${user_input}/old_db.esl"
+    efi-readvar -v dbx -o "${user_input}/old_dbx.esl"
+    read -n 1 -s -r -p "Old keys successfully read into files, press any key to continue.."
+
+    # Generate keys and certificates
+    echo "generating keys & certificates..."
+    openssl req -new -x509 -newkey rsa:2048 -keyout "${user_input}/PK.key" -out "${user_input}/PK.crt" -days 3650 -nodes -sha256
+    openssl req -new -x509 -newkey rsa:2048 -keyout "${user_input}/KEK.key" -out "${user_input}/KEK.crt" -days 3650 -nodes -sha256
+    openssl req -new -x509 -newkey rsa:2048 -keyout "${user_input}/db.key" -out "${user_input}/db.crt" -days 3650 -nodes -sha256
+    # Change permissions to read-only for root (precaution)
+    sudo chmod -v 400 "${user_input}/PK.key"
+    sudo chmod -v 400 "${user_input}/KEK.key"
+    sudo chmod -v 400 "${user_input}/db.key"
+    read -n 1 -s -r -p "Keys successfully generated, press any key to continue.."
+
+    # Create update files
+    echo "creating update files for keystore.."
+    cert-to-efi-sig-list -g "$(uuidgen)" "${user_input}/PK.crt" "${user_input}/PK.esl"
+    sign-efi-sig-list -k "${user_input}/PK.key" -c "${user_input}/PK.crt" PK "${user_input}/PK.esl" "${user_input}/PK.auth"
+    cert-to-efi-sig-list -g "$(uuidgen)" "${user_input}/KEK.crt" "${user_input}/KEK.esl"
+    sign-efi-sig-list -a -k "${user_input}/PK.key" -c "${user_input}/PK.crt" KEK "${user_input}/KEK.esl" "${user_input}/KEK.auth"
+    cert-to-efi-sig-list -g "$(uuidgen)" "${user_input}/db.crt" "${user_input}/db.esl"
+    sign-efi-sig-list -a -k "${user_input}/KEK.key" -c "${user_input}/KEK.crt" db "${user_input}/db.esl" "${user_input}/db.auth"
+    sign-efi-sig-list -k "${user_input}/KEK.key" -c "${user_input}/KEK.crt" dbx "${user_input}/old_dbx.esl" "${user_input}/old_dbx.auth"
+    read -n 1 -s -r -p "Update files successfully generated, press any key to continue.."
+
+    # Create DER (Distinguished Encoding Rules) files, needed for some BIOSes
+    openssl x509 -outform DER -in "${user_input}/PK.crt" -out "${user_input}/PK.cer"
+    openssl x509 -outform DER -in "${user_input}/KEK.crt" -out "${user_input}/KEK.cer"
+    openssl x509 -outform DER -in "${user_input}/db.crt" -out "${user_input}/db.cer"
+    read -n 1 -s -r -p "DER versions successfully generated, press any key to continue"
+
+    # Create compound esl files & auth counterparts
+    cat "${user_input}/old_KEK.esl" "${user_input}/KEK.esl" > "${user_input}/compound_KEK.esl"
+    cat "${user_input}/old_db.esl" "${user_input}/db.esl" > "${user_input}/compound_db.esl"
+    sign-efi-sig-list -k "${user_input}/PK.key" -c "${user_input}/PK.crt" KEK "${user_input}/compound_KEK.esl" "${user_input}/compound_KEK.auth"
+    sign-efi-sig-list -k "${user_input}/KEK.key" -c "${user_input}/KEK.crt" db "${user_input}/compound_db.esl" "${user_input}/compound_db.auth"
+    read -n 1 -s -r -p "New esl & auth files successfully generated! See Sakaki's guide to update your keystore! (press any key to continue)"
+  else
+    ERROR_MSG="invalid directory, please exit and create new directory (check permissions!).."
+    return 1
+  fi
+
+  return 0
+}
 
 
 ## Check for signing keys
 # Check if files specified in config
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 if [[ -f "$SCRIPT_DIR/keylocations.cfg" ]];then
+  # Import config file
   source "$SCRIPT_DIR/keylocations.cfg"
 
+  # Check keys exist
+  if [[ -z "$key_location" || -z "$cert_location" ]]; then
+    # empty key & cert file locations in config
+    valid_keys="False"
+  else
+    # Key & cert files are specified
+    valid_keys="True"
+
+    # Check key & cert valid locations
+    if [[ ! -f $key_location || ! -f $cert_location ]]; then
+      # Otherwise print error
+      echo "invalid Signature Database key/certificate locations, check config file.."
+      exit 1
+    fi
+  fi
+
   # Check valid locations
-  if [[ -z $key_location ]] || [[ -z $cert_location ]] || [[ -z $kernel_location ]]; then
+  if [[ -z $kernel_location ]]; then
     # Otherwise print error
-    echo "malformed config file.."
+    echo "empty kernel location, check config file.."
     exit 1
+  else
+    # Check files exist
+    if [[ ! -d $kernel_location ]]; then
+      # Otherwise print error
+      echo "missing kernel location, check config file.."
+      exit 1
+    fi
   fi
 else
   # Otherwise print error
   echo "missing config file.."
-  exit 1
-fi
-
-# Check files exist
-if [[ ! -f $key_location ]] || [[ ! -f $cert_location ]]; then
-  # Otherwise print error
-  echo "missing signing key/certificate, check config file.."
-  exit 1
-fi
-if [[ ! -d $kernel_location ]]; then
-  # Otherwise print error
-  echo "missing kernel location, check config file.."
   exit 1
 fi
 
@@ -161,26 +231,34 @@ while [[ "$stop" == "False" ]]; do
   for k in "${skernels[@]}"; do
     echo "  $k"
   done
-
+  if [[ "$valid_keys" == "True" ]]; then
+    echo "Signature Database key & certificate detected.."
+  else
+    echo "Signature Database key and/or certificate not detected.."
+  fi
   echo "=============================================="
   echo "$prev_out"
   echo "=============================================="
   echo "1 - Sign a kernel"
   echo "2 - Purge signed kernel"
-  echo "3 - Install/Remove unsigned kernel"
-  echo "4 - Modify Grub"
-  echo "5 - Reboot"
+  echo "3 - Create new keys"
+  echo "4 - Install/Remove unsigned kernel"
+  echo "5 - Modify Grub"
+  echo "6 - Reboot"
   echo "0 - Exit"
-  echo "enter input:"
-  read -r user_input
+  read -r user_input -p "enter input:"
 
   if [[ "$user_input" == "1" ]]; then
-    # sign kernels
-    sign_kernel
-    if [[ $? == 0 ]]; then
-      prev_out="success!"
+    if [[ "$valid_keys" == "True" ]]; then
+      # sign kernels
+      sign_kernel
+      if [[ $? == 0 ]]; then
+        prev_out="success!"
+      else
+        prev_out="failure: $ERROR_MSG"
+      fi
     else
-      prev_out="failure: $ERROR_MSG"
+      prev_out="create new keys and append to existing/default keys first!"
     fi
   elif [[ "$user_input" == "2" ]]; then
     # purge kernels
@@ -191,6 +269,14 @@ while [[ "$stop" == "False" ]]; do
       prev_out="failure: $ERROR_MSG"
     fi
   elif [[ "$user_input" == "3" ]]; then
+    # create keys
+    create_keys
+    if [[ $? == 0 ]]; then
+      prev_out="success!"
+    else
+      prev_out="failure: $ERROR_MSG"
+    fi
+  elif [[ "$user_input" == "4" ]]; then
     # redirect to mainline-gtk app
     mainline-gtk
     if [[ $? == 0 ]]; then
@@ -198,7 +284,7 @@ while [[ "$stop" == "False" ]]; do
     else
       prev_out="failure: $?"
     fi
-  elif [[ "$user_input" == "4" ]]; then
+  elif [[ "$user_input" == "5" ]]; then
     # redirect to grub-customizer app
     grub-customizer
     if [[ $? == 0 ]]; then
@@ -206,7 +292,7 @@ while [[ "$stop" == "False" ]]; do
     else
       prev_out="failure: $?"
     fi
-  elif [[ "$user_input" == "5" ]]; then
+  elif [[ "$user_input" == "6" ]]; then
     reboot
   elif [[ "$user_input" == "0" ]]; then
     # exit
